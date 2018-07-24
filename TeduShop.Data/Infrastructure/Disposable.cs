87e5b171cab969b664fdbedff7ec4f5d031ecc2f
DESKTOP-DMPF34Q\My PC <kha.dang.2191992@gmail.com>
2018-07-24 17:22:42 +0700

    Bai_6 end : create Infrastructure to TeduShop.Data

---
+++ TeduShop.Data/Infrastructure/Disposable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TeduShop.Data.Infrastructure
+{
+    public class Disposable : IDisposable
+    {
+        private bool isDisposed;
+
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!isDisposed && disposing)
+            {
+                DisposeCore();
+            }
+            isDisposed = true;
+        }
+
+        //Overide this to dispose custom object
+        protected virtual void DisposeCore() { }
+
+        //public void Dispose()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
